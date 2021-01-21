187231dbd8cea9f5129406405cca994a4cccd15a
lime <osmium760@gmail.com>
2021-01-21 22:44:43 +0300

    add coc-installer

---
+++ .config/nvim/coc-installer.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+
+# Install latest nodejs
+if [ ! -x "$(command -v node)" ]; then
+    curl --fail -LSs https://install-node.now.sh/latest | sh
+    export PATH="/usr/local/bin/:$PATH"
+fi
+
+# Use package feature to install coc.nvim
+
+# for vim8
+# mkdir -p ~/.vim/pack/coc/start
+# cd ~/.vim/pack/coc/start
+# curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+
+# for neovim
+mkdir -p ~/.local/share/nvim/site/pack/coc/start
+cd ~/.local/share/nvim/site/pack/coc/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+
+# Install extensions
+mkdir -p ~/.config/coc/extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+  echo '{"dependencies":{}}'> package.json
+fi
+# Change extension names to the extensions you need
+npm install coc-snippets coc-sh coc-html coc-explorer \
+  coc-yaml coc-tsserver coc-python coc-json coc-css \
+  --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
