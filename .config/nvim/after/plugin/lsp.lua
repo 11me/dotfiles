342527f539def878433f7c827272d58f3f33632f
11me <11me>
2023-02-28 19:43:37 +0300

    Disable autocompletion

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -26,24 +26,27 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+	['<C-Space>'] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+    mapping = cmp_mappings,
+    completion = {
+        autocomplete = false
+    }
 })
 
 lsp.set_preferences({
-	suggest_lsp_servers = false,
-	sign_icons = {
-		error = 'E',
-		warn = 'W',
-		hint = 'H',
-		info = 'I'
-	}
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn  = 'W',
+        hint  = 'H',
+        info  = 'I'
+    }
 })
 
 lsp.on_attach(function(client, bufnr)
