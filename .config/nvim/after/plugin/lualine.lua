a283e77c64b5c2e55ff57ac03c6b4e860bc6cb6b
Emil Khamdamov <osmium760@gmail.com>
2023-05-10 07:03:41 +0000

    show fullpath

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -49,7 +49,13 @@ require('lualine').setup {
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = {
+            {
+                'filename',
+                file_status = true,
+                path = 2,
+            }
+        },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
