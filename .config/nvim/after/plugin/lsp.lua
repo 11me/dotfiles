c204227a809d56b577c80534c1011e8589945f92
11me <osmium760@gmail.com>
2024-09-17 21:12:44 +0300

    disable nvim autocomplete

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,28 +3,28 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'tsserver',
-	'gopls',
+    'tsserver',
+    'gopls',
 })
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua-language-server', {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim' }
-			}
-		}
-	}
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-x><C-o>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-x><C-o>'] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
@@ -32,9 +32,9 @@ cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings,
-    --completion = {
-    --    autocomplete = false,
-    --}
+    completion = {
+        autocomplete = false,
+    }
 })
 
 lsp.set_preferences({
@@ -48,22 +48,21 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set('n', '<leader>lws', function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set('n', '<leader>lca', function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set('n', '<leader>lrr', function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set('n', '<leader>lrn', function() vim.lsp.buf.rename() end, opts)
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>lws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>lca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>lrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>lrn', function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float)
-	vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-	virtual_text = true
+    virtual_text = true
 })
-
